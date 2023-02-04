2eb0204e93d266772edf39691f16a7c746885913
Piero Borrelli <piero@techcopyteam.com>
2023-02-04 18:29:33 +0100

    Add todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React from 'react'
 
 import './App.css';
 
+const ToDoList = ({ todos }) => {
+  return (
+    <ul>
+    {todos.map((todo) => <li>{todo}</li>)}
+    </ul>
+  )
+}
+
 function App() {
+  const [value, setValue] = React.useState();
+  const [todos, setTodos] = React.useState([])
+
+  function addTodo(todo) {
+    setTodos((oldTodos) => [...oldTodos, todo])
+    setValue("")
+  }
+  return (
+    <div>
+      <h1>To do list</h1>
+      <input type="text" placeholder="Add something to do" value={value} onChange={(e) => setValue(e.target.value)}></input>
+      <button onClick={() => addTodo(value)}>Add</button>
+      <ToDoList todos={todos}/>
+    </div>
+  )
 
 }
 
